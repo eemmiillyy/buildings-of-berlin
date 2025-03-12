c2d751111ac69d778e3f2a788d82ec9b011e4ed1
Emily <emily.morgan@code.berlin>
2025-03-12 12:05:32 +0100

    cors

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://buildings-of-berlin-be.netlify.app/api',
+  headers: {
+    'access-control-allow-origin': '*',
+  },
 });
 
 export default apiClient;
