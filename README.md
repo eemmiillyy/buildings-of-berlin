0a635e6775aab8a3a0233725a3a508e8a62a0345
Emily <emily.morgan@code.berlin>
2025-03-12 12:41:27 +0100

    potentially revert

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 npm install
-cd server npm start
+cd server npm dev
 cd client npm start
 
 This will start:
-The server on http://localhost:3000
+The server on http://localhost:8888
 The client on http://localhost:8080
 
 
--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
     ? 'https://buildings-of-berlin-be.netlify.app/.netlify/functions/api' 
-    : 'http://localhost:3000/api'
+    : 'http://localhost:8888/api'
 });
 
 export default apiClient;
