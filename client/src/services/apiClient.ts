40c235e43f1a9141748c1dd81d99d5267bc21663
Emily <emily.morgan@code.berlin>
2025-03-12 12:20:48 +0100

    test

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-  ? 'https://buildings-of-berlin-be.netlify.app/.netlify/functions/api' 
+  ? 'https://buildings-of-berlin-be.netlify.app/api' 
   : 'http://localhost:3000/api',
   withCredentials: true,
 });
